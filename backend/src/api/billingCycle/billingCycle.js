7e31c5f5bcf9042b46d4be480b2b9949a6b61bba
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-23 10:00:32 -0300

    Tratamento das mensagens de erro e adição da rota count

--- backend/src/api/billingCycle/billingCycle.js
+++ backend/src/api/billingCycle/billingCycle.js
@@ -3,16 +3,16 @@ const mongoose = restful.mongoose
 
 const creditSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true }
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do crédito!'] }
 })
 
 const debtSchema = new mongoose.Schema({
     name: { type: String, required: true },
-    value: { type: Number, min: 0, required: true },
+    value: { type: Number, min: 0, required: [true, 'Informe o valor do débito!'] },
     status: { type: String, required: false, uppercase: true, enum: ['PAGO', 'PENDENTE', 'AGENDADO'] }
 })
 
-const billingCycle = new mongoose.Schema({
+const billingCycleSchema = new mongoose.Schema({
     name: { type: String, required: true },
     month: { type: Number, min: 0, max: 12, required: true },
     year: { type: Number, min: 1970, max: 2100, required: true },
--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -7,4 +7,14 @@ Usamos o new para pegar a versão mais recente
 e o runValidators para rodar as validações */
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+BillingCycle.route('count', (req, res, next) => {
+    BillingCycle.count((error, value) => {
+        if (error) {
+            res.status(500).json({ errors: [error] })
+        } else {
+            res.json({ value })
+        }
+    })
+})
+
 module.exports = BillingCycle
