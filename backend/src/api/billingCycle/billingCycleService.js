9261fb73a744bde6f46bbac0e7561d60cd03a0e3
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-23 10:40:51 -0300

    Inclusão do summary

--- backend/src/api/billingCycle/billingCycleService.js
+++ backend/src/api/billingCycle/billingCycleService.js
@@ -7,6 +7,7 @@ Usamos o new para pegar a versão mais recente
 e o runValidators para rodar as validações */
 BillingCycle.updateOptions({ new: true, runValidators: true })
 
+/* Rota para recuperar o total de registros salvos */
 BillingCycle.route('count', (req, res, next) => {
     BillingCycle.count((error, value) => {
         if (error) {
@@ -15,6 +16,28 @@ BillingCycle.route('count', (req, res, next) => {
             res.json({ value })
         }
     })
+})
+
+/* Rota para recuperar a soma dos valores de débito e crédito para efetuar
+os futuros cálculos, através no método aggregate ($project e $group) */
+BillingCycle.route('summary', (req, res, next) => {
+    BillingCycle.aggregate([
+        {
+            $project: { credit: { $sum: "$credits.value" }, debt: { $sum: "$debts.value" } }
+        },
+        {
+            $group: { _id: null, credit: { $sum: "$credit" }, debt: { $sum: "$debt" } }
+        },
+        {
+            $project: { _id: 0, credit: 1, debt: 1 } // Booleano
+        }]).exec((error, result) => {
+            if (error) {
+                res.status(500).json({ errors: [error] })
+            } else {
+                // Caso não hajam registros na tabela, o crédito/débito vão ser retornados como 0
+                res.json(result[0] || { credit: 0, debt: 0 })
+            }
+        })
 })
 
 module.exports = BillingCycle
