af1163aa8eb86581819f5ae0987a2dbc02d1c65e
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-24 10:34:45 -0300

    Criação do header, sidebar e menu

---
+++ frontend/src/common/template/menu.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default props => (
+    <ul className="sidebar-menu">
+        
+    </ul>
+)
--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -1,8 +1,12 @@
 import '../common/template/dependencies'
 import React from 'react'
 
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+
 export default props => (
     <div className="wrapper">
-        <h1>App</h1>
+        <Header />
+        <Sidebar />
     </div>
 )
