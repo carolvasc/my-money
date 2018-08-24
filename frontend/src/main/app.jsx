ed8c0dbaeeb38a561bef2d5e1e63b53502b4457a
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-24 11:15:13 -0300

    Implementação das rotas

--- frontend/src/main/app.jsx
+++ frontend/src/main/app.jsx
@@ -4,13 +4,14 @@ import React from 'react'
 import Header from '../common/template/header'
 import Sidebar from '../common/template/sidebar'
 import Footer from '../common/template/footer'
+import Routes from  './routes'
 
 export default props => (
     <div className="wrapper">
         <Header />
         <Sidebar />
         <div className="content-wrapper">
-            <h1>Conteudo</h1>
+            <Routes />
         </div>
         <Footer />
     </div>
