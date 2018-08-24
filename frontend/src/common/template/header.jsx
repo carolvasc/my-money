6b54ef11833ec8fff4155aa5a4f2046a3bb38bdb
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-24 10:47:32 -0300

    Finalização do menu

--- frontend/src/common/template/header.jsx
+++ frontend/src/common/template/header.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default props => (
     <header className="main-header">
         <a href="/#/" className="logo">
-            <span className="logo-mini"><b>My</b>M</span>
+            <span className="logo-mini"><b>My</b><i className="fa fa-usd"></i></span>
             <span className="logo-lg">
                 <i className="fa fa-money"></i>
                 <b> My</b> Money
