530e07d4b149421beebcd05e6545c65b6c8dcde0
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-24 12:03:55 -0300

    ValueBox no Dashboard

--- frontend/src/dashboard/dashboard.jsx
+++ frontend/src/dashboard/dashboard.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 import ContentHeader from '../common/template/contentHeader'
 import Content from '../common/template/content'
+import ValueBox from '../common/widget/valueBox'
+import Row from '../common/layout/row'
 
 class Dashboard extends React.Component {
     render() {
@@ -9,7 +11,11 @@ class Dashboard extends React.Component {
             <div>
                 <ContentHeader title='Dashboard' subtitle='Versão 1.0' />
                 <Content>
-                    Dashboard
+                    <Row>
+                        <ValueBox cols='12 4' color='green' icon='bank' value='R$ 10' text='Total de Créditos' />
+                        <ValueBox cols='12 4' color='red' icon='credit-card' value='R$ 10' text='Total de Débitos' />
+                        <ValueBox cols='12 4' color='blue' icon='money' value='R$ 10' text='Valor Consolidado' />
+                    </Row>
                 </Content>
             </div>
         )
