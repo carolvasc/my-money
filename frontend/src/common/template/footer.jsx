b231bbd104f99ffd4258c251d0ac635a13de4a70
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-24 10:59:19 -0300

    Criação do footer e finalização do template

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default props => (
+    <footer className="main-footer">
+        <strong>
+            Curso da <strong> Cod<span className='text-danger'>3</span>r </strong>
+            adaptado com <i className="fa fa-heart text-danger"></i> por
+            <strong> Carolina Vasconcelos</strong>
+        </strong>
+    </footer>
+)
