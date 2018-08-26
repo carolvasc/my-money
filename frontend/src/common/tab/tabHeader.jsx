fd74332049f387ad7a3657f4ffea49d47391e368
Carolina Vasconcelos <carol.vasconcelos080@gmail.com>
2018-08-26 18:53:11 -0300

    Implementação do Redux nas Tabs

--- frontend/src/common/tab/tabHeader.jsx
+++ frontend/src/common/tab/tabHeader.jsx
@@ -1,11 +1,17 @@
 import React from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+
+import { selectTab } from './tabActions'
 
 class TabHeader extends React.Component {
     render() {
+        const selected = this.props.tab.selected === this.props.target
         return (
-            <li>
+            <li className={selected ? 'active' : ''}>
                 <a href='javascript:;'
                     data-toggle='tab'
+                    onClick={() => this.props.selectTab(this.props.target)}
                     data-target={this.props.target}>
                     <i className={`fa fa-${this.props.icon}`}
                         style={{ paddingRight: '4px' }}></i>{this.props.label}
@@ -15,4 +21,6 @@ class TabHeader extends React.Component {
     }
 }
 
-export default TabHeader
+const mapStateToProps = store => ({ tab: store.tab })
+const mapDispatchToProps = dispatch => bindActionCreators({ selectTab }, dispatch)
+export default connect(mapStateToProps, mapDispatchToProps)(TabHeader)
