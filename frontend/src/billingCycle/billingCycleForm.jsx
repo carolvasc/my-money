8f115740e9ed75cf520e585cde54e60447b00eb5
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-27 16:13:17 -0300

    Melhorias nos inputs e labels de inclusão

--- frontend/src/billingCycle/billingCycleForm.jsx
+++ frontend/src/billingCycle/billingCycleForm.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { reduxForm, Field } from 'redux-form'
+import labelAndInput from '../common/form/labelAndInput'
 
 class BillingCycleForm extends React.Component {
     render() {
@@ -7,9 +8,12 @@ class BillingCycleForm extends React.Component {
         return (
             <form role='form' onSubmit={handleSubmit}>
                 <div className="box-body">
-                    <Field name='name' component='input' />
-                    <Field name='month' component='input' />
-                    <Field name='year' component='input' />
+                    <Field name='name' component={labelAndInput} autoComplete='off'
+                        label='Nome' cols='12 6' placeholder='Informe uma descrição' />
+                    <Field name='month' component={labelAndInput} type='number' autoComplete='off'
+                        label='Mês' cols='12 3' placeholder='Informe o mês' />
+                    <Field name='year' component={labelAndInput} type='number' autoComplete='off'
+                        label='Ano' cols='12 3' placeholder='Informe o ano' />
                 </div>
                 <div className="box-footer">
                     <button type='submit' className="btn btn-primary">Submit</button>
