735ad06cc8b5ba8c0e6b72d6d59bf851acc0974d
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-27 15:24:05 -0300

    Mensagens de validação

--- frontend/src/billingCycle/billingCycleActions.js
+++ frontend/src/billingCycle/billingCycleActions.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { toastr } from 'react-redux-toastr'
 const BASE_URL = 'http://localhost:3003/api'
 
 export function getList() {
@@ -11,6 +12,12 @@ export function getList() {
 
 export function create(values) {
     axios.post(`${BASE_URL}/billingCycles`, values)
+        .then(resp => {
+            toastr.success('Sucesso', 'Operação realizada com sucesso.')
+        })
+        .catch(err => {
+            err.response.data.errors.forEach(error => toastr.error('Erro', error))
+        })
     return {
         type: 'TEMP'
     }
--- frontend/src/main/reducers.js
+++ frontend/src/main/reducers.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux'
 import { reducer as formReducer } from 'redux-form'
+import { reducer as toastrReducer } from 'react-redux-toastr'
 
 import DashboardReducer from '../dashboard/dashboardReducer'
 import TabReducer from '../common/tab/tabReducer'
@@ -9,7 +10,8 @@ const rootReducer = combineReducers({
     dashboard: DashboardReducer,
     tab: TabReducer,
     billingCycle: BillingCycleReducer,
-    form: formReducer
+    form: formReducer,
+    toastr: toastrReducer
 })
 
 export default rootReducer
