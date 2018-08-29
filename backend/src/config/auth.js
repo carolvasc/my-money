0ea448fb789361bf0f93c16e05770dbcf8200617
Carolina Vasconcelos <cvsouza04@gmail.com>
2018-08-29 11:52:06 -0300

    Configuração do auth

---
+++ backend/src/config/auth.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken')
+const env = require('../../env')
+
+module.exports = (req, res, next) => {
+    // Caso seja uma requisição do tipo OPTIONS, o authSecret não autenticará o token
+    if (req.method === 'OPTIONS') {
+        next()
+    } else {
+        const token = req.body.token || req.query.token || req.headers['authorization']
+
+        if (!token) {
+            return res.status(403).send({ errors: ['No token provided.'] })
+        }
+
+        jwt.verify(token, env.authSecret, function (err, decoded) {
+            if (err) {
+                return res.status(403).send({
+                    errors: ['Falha ao autenticar o token']
+                })
+            } else {
+                // Caso eu queira utilizar esse token decodificado em outros lugares
+                //req.decoded = decoded
+                next()
+            }
+        })
+    }
+}
