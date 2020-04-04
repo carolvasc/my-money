a0150b0bd8e550bff3b6ca63a637c216ac5bc59e
Carol Vasconcelos <cvsouza04@gmail.com>
2020-04-04 18:14:24 -0300

    Updates

---
+++ main/app.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import Header from '../common/template/header'
+import Sidebar from '../common/template/sidebar'
+import Footer from '../common/template/footer'
+import Messages from '../common/msg/messages'
+
+export default props => (
+    <div className="wrapper">
+        <Header />
+        <Sidebar />
+        <div className="content-wrapper">
+            {props.children}
+        </div>
+        <Footer />
+        <Messages />
+    </div>
+)
